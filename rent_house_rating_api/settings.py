7b101ba5a536ccefd702063702d123e4e59b5714
PeterChen <sanpeter992000@gmail.com>
2021-08-15 15:00:24 +0800

    add location app in INSTALLED_APPS

--- rent_house_rating_api/settings.py
+++ rent_house_rating_api/settings.py
@@ -35,7 +35,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'apps.system.apps.SystemConfig'
+    'apps.system.apps.SystemConfig',
+    'apps.location.apps.LocationConfig'
 ]
 
 MIDDLEWARE = [
