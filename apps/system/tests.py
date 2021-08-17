081eac96caa6c207f6b4d0573640de9d83efc267
PeterChen <sanpeter992000@gmail.com>
2021-08-17 21:10:18 +0800

    change status code number to status in rest_framework

--- apps/system/tests.py
+++ apps/system/tests.py
@@ -1,4 +1,5 @@
 from tests.setting.setting import Roles
+from rest_framework import status
 
 
 class User(object):
@@ -7,7 +8,8 @@ class User(object):
     def test_get_users(self):
         res = self.client.get(self.user_url)
         if self.role == Roles.SUPERUSER_ROLE_NAME:
-            if res.status_code != 200 and res.status_code != 204:
+            if res.status_code != status.HTTP_200_OK and \
+                    res.status_code != status.HTTP_204_NO_CONTENT:
                 raise Exception(f'回傳status_code:{res.status_code}')
         elif self.role == Roles.NORMAL_USER_ONE_ROLE_NAME:
-            self.assertEqual(res.status_code, 403)
+            self.assertEqual(res.status_code, status.HTTP_403_FORBIDDEN)
