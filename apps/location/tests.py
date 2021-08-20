cd44423d2455640906bc68473b0d9340636061ce
PeterChen <sanpeter992000@gmail.com>
2021-08-20 10:35:53 +0800

    add test_get_countries and test_create_country

--- apps/location/tests.py
+++ apps/location/tests.py
@@ -1,3 +1,21 @@
-from django.test import TestCase
+from tests.setting.setting import Roles
+from rest_framework import status
 
-# Create your tests here.
+
+class Country(object):
+    country_url = '/location/countries'
+
+    def test_get_countries(self):
+        res = self.client.get(self.country_url)
+        if res.status_code != status.HTTP_200_OK and \
+                res.status_code != status.HTTP_204_NO_CONTENT:
+            raise Exception(f'回傳status_code:{res.status_code}')
+
+    def test_create_country(self):
+        country_name = 'Japan'
+        res = self.client.post(self.country_url,
+                               {'name': country_name})
+        if self.role == Roles.SUPERUSER_ROLE_NAME:
+            self.assertEqual(res.status_code, status.HTTP_201_CREATED)
+        elif self.role == Roles.NORMAL_USER_ONE_ROLE_NAME:
+            self.assertEqual(res.status_code, status.HTTP_403_FORBIDDEN)
