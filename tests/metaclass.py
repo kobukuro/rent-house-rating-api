06d99d3a669eda4842de473d02e49f0f984ce462
PeterChen <sanpeter992000@gmail.com>
2021-08-21 22:45:44 +0800

    only method whose name starts with "test" will be renamed

--- tests/metaclass.py
+++ tests/metaclass.py
@@ -5,7 +5,8 @@ class AvoidSameMethodNameMetaclass(type):
     def __new__(self, class_name, bases, attrs):
         new_attrs = {}
         for name, value in attrs.items():
-            if not name.startswith('__') and isinstance(value, types.FunctionType):
+            # test開頭的method才改名
+            if not name.startswith('__') and isinstance(value, types.FunctionType) and name.startswith('test'):
                 new_attrs[name + '_' + attrs['__module__'] + '.' + class_name] = value
             else:
                 new_attrs[name] = value
