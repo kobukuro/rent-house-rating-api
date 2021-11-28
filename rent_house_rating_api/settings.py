53773ca188a78a4071f5fcf2a27fb6c1468b2061
PeterChen <sanpeter992000@gmail.com>
2021-11-28 21:47:44 +0800

    change SECRET_KEY to environment variable

--- rent_house_rating_api/settings.py
+++ rent_house_rating_api/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -19,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3sedrj%kpn5fmjxzzy%&0g6t5yc0@90p_skjv*jj3b0tkuz3yp'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
