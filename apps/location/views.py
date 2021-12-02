da237085f553f675d658e7756325084d133b39fb
PeterChen <sanpeter992000@gmail.com>
2021-12-02 22:25:43 +0800

    change to simplejwt

--- apps/location/views.py
+++ apps/location/views.py
@@ -1,216 +1,59 @@
 from rest_framework.views import APIView
-from apps.location.models import Country, Location, Rating
-from apps.system.models import User
-from apps.location.serializers import CountrySerializer, LocationSerializer, RatingSerializer
 from rest_framework.response import Response
 from rest_framework import status
+from apps.system.serializers import UserSerializer
+from rest_framework_jwt.views import ObtainJSONWebTokenView
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+from apps.system.models import User
 from rent_house_rating_api.permission_class import CustomPermissionClass
-from django.http import Http404
-from django.db import IntegrityError
 
 
-class CountryList(APIView):
-    permission_classes = [CustomPermissionClass(api_name=__qualname__)]
-
-    def get(self, request):
-        countries = Country.objects.all()
-        serializer = CountrySerializer(countries, many=True)
-        if not serializer.data:
-            return Response(serializer.data, status=status.HTTP_204_NO_CONTENT)
-        else:
-            return Response(serializer.data)
+class RegisterUserView(APIView):
+    # 此view不需要Authorization 因為是註冊 還沒有辦法登入
+    authentication_classes = []
+    permission_classes = []
 
     def post(self, request):
-        serializer = CountrySerializer(data=request.data)
+        serializer = UserSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-class CountryDetail(APIView):
-    permission_classes = [CustomPermissionClass(api_name=__qualname__)]
+# class CustomObtainJSONWebToken(ObtainJSONWebTokenView):
+#     def post(self, request, *args, **kwargs):
+#         parent_result = super().post(request, *args, **kwargs)
+#         parent_result_status_code = super().post(request, *args, **kwargs).status_code
+#         # 成功登入要update users_user table的last_login欄位
+#         if parent_result_status_code == status.HTTP_201_CREATED:
+#             user = User.objects.get(email=request.data['email'])
+#             from django.utils import timezone
+#             user.last_login = timezone.now()
+#             user.save()
+#             parent_result.data['username'] = user.username
+#         return parent_result
 
-    def get_object(self, pk):
-        try:
-            return Country.objects.get(pk=pk)
-        except Country.DoesNotExist:
-            raise Http404
+class CustomObtainJSONWebToken(TokenObtainPairView):
+    def post(self, request, *args, **kwargs):
+        parent_result = super().post(request, *args, **kwargs)
+        parent_result_status_code = super().post(request, *args, **kwargs).status_code
+        # 成功登入要update users_user table的last_login欄位
+        if parent_result_status_code == status.HTTP_200_OK:
+            user = User.objects.get(email=request.data['email'])
+            from django.utils import timezone
+            user.last_login = timezone.now()
+            user.save()
+            parent_result.data['username'] = user.username
+        return parent_result
 
-    def get(self, request, pk):
-        country = self.get_object(pk)
-        serializer = CountrySerializer(country)
-        return Response(serializer.data)
-
-    def put(self, request, pk):
-        country = self.get_object(pk)
-        serializer = CountrySerializer(country, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, pk):
-        country = self.get_object(pk)
-        country.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
-
-
-class LocationList(APIView):
-    permission_classes = [CustomPermissionClass(api_name=__qualname__)]
-
-    def get(self, request):
-        locations = Location.objects.all()
-        serializer = LocationSerializer(locations, many=True)
-        return Response(serializer.data)
-
-    def post(self, request):
-        user = User.objects.get(id=request.user.id)
-        serializer = LocationSerializer(data=request.data)
-        if serializer.is_valid():
-            try:
-                country_id = request.data['country_id']
-                serializer.save(country_id=country_id, created_by=user)
-            except IntegrityError as e:
-                serializer.error_messages = str(e)
-                return Response(serializer.error_messages,
-                                status=status.HTTP_400_BAD_REQUEST)
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-class LocationDetail(APIView):
-    permission_classes = [CustomPermissionClass(api_name=__qualname__)]
-
-    def get_object(self, pk):
-        try:
-            return Location.objects.get(pk=pk)
-        except Location.DoesNotExist:
-            raise Http404
-
-    def get(self, request, pk):
-        location = self.get_object(pk)
-        serializer = LocationSerializer(location)
-        return Response(serializer.data)
-
-    def put(self, request, pk):
-        location = self.get_object(pk)
-        if request.user.is_superuser:
-            serializer = LocationSerializer(location, data=request.data)
-            if serializer.is_valid():
-                serializer.save()
-                return Response(serializer.data)
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-        else:
-            if location.created_by_id != request.user.id:
-                return Response({'detail': 'You can not modify the location which is not created by you.'},
-                                status=status.HTTP_403_FORBIDDEN)
-            # 這個location底下，除了自己以外的rating
-            ratings = Rating.objects.filter(location_id=pk).exclude(created_by=request.user.id)
-            if ratings:  # 這個location底下有不是自己的rating
-                return Response({'detail': 'There is rating of other people under this location, so you can not modify.'},
-                                status=status.HTTP_412_PRECONDITION_FAILED)
-            else:  # 這個location底下目前只有自己的rating
-                serializer = LocationSerializer(location, data=request.data)
-                if serializer.is_valid():
-                    serializer.save()
-                    return Response(serializer.data)
-                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def patch(self, request, pk):
-        location = self.get_object(pk)
-        if request.user.is_superuser:
-            serializer = LocationSerializer(location, data=request.data, partial=True)
-            if serializer.is_valid():
-                serializer.save()
-                return Response(serializer.data)
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-        else:
-            if location.created_by_id != request.user.id:
-                return Response({'detail': 'You can not modify the location which is not created by you.'},
-                                status=status.HTTP_403_FORBIDDEN)
-            # 這個location底下，除了自己以外的rating
-            ratings = Rating.objects.filter(location_id=pk).exclude(created_by=request.user.id)
-            if ratings:  # 這個location底下有不是自己的rating
-                return Response({'detail': 'There is rating of other people under this location, so you can not modify.'},
-                                status=status.HTTP_412_PRECONDITION_FAILED)
-            else:  # 這個location底下目前只有自己的rating
-                serializer = LocationSerializer(location, data=request.data, partial=True)
-                if serializer.is_valid():
-                    serializer.save()
-                    return Response(serializer.data)
-                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-class RatingList(APIView):
+class UserList(APIView):
     permission_classes = [CustomPermissionClass(api_name=__qualname__)]
 
     def get(self, request):
-        ratings = Rating.objects.all()
-        serializer = RatingSerializer(ratings, many=True)
+        users = User.objects.all()
+        serializer = UserSerializer(users, many=True)
         return Response(serializer.data)
-
-    def post(self, request):
-        user = User.objects.get(id=request.user.id)
-        serializer = RatingSerializer(data=request.data)
-        if serializer.is_valid():
-            try:
-                serializer.save(location_id=request.data['location_id'], created_by=user)
-            except IntegrityError as e:
-                serializer.error_messages = str(e)
-                return Response(serializer.error_messages, status=status.HTTP_400_BAD_REQUEST)
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-class RatingDetail(APIView):
-    permission_classes = [CustomPermissionClass(api_name=__qualname__)]
-
-    def get_object(self, pk):
-        try:
-            return Rating.objects.get(pk=pk)
-        except Rating.DoesNotExist:
-            raise Http404
-
-    def get(self, request, pk):
-        rating = self.get_object(pk)
-        serializer = RatingSerializer(rating)
-        return Response(serializer.data)
-
-    def put(self, request, pk):
-        rating = self.get_object(pk)
-        if request.user.is_superuser:
-            serializer = RatingSerializer(rating, data=request.data)
-            if serializer.is_valid():
-                serializer.save()
-                return Response(serializer.data)
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-        else:
-            # 只能改自己寫的rating資料
-            if rating.created_by.id != request.user.id:
-                return Response(status=status.HTTP_403_FORBIDDEN)
-            else:
-                serializer = RatingSerializer(rating, data=request.data)
-                if serializer.is_valid():
-                    serializer.save()
-                    return Response(serializer.data)
-                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def patch(self, request, pk):
-        rating = self.get_object(pk)
-        if request.user.is_superuser:
-            serializer = RatingSerializer(rating, data=request.data, partial=True)
-            if serializer.is_valid():
-                serializer.save()
-                return Response(serializer.data)
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-        else:
-            # 只能改自己寫的rating資料
-            if rating.created_by.id != request.user.id:
-                return Response(status=status.HTTP_403_FORBIDDEN)
-            else:
-                serializer = RatingSerializer(rating, data=request.data, partial=True)
-                if serializer.is_valid():
-                    serializer.save()
-                    return Response(serializer.data)
-                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
