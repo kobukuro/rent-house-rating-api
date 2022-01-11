2f30a4103a3378fdaaf96f08d019b6d1aead1e82
PeterChen <sanpeter992000@gmail.com>
2022-01-11 17:56:34 +0800

    add created_at field in RatingSerializer

--- apps/location/serializers.py
+++ apps/location/serializers.py
@@ -36,4 +36,4 @@ class RatingSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Rating
-        fields = ['id', 'rating', 'comment', 'location_id', 'created_by_username']
+        fields = ['id', 'rating', 'comment', 'location_id', 'created_by_username', 'created_at']
